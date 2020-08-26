5e3d2452f7eab3bee5d0dc7284b15338024787f1
akrolayer <almagestf532@gmail.com>
2020-08-27 00:30:06 +0900

    Initiarize commit

---
+++ AudioRecorder/AudioRecorder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioRecorder
+//
+//  Created by akrolayer on 2020/08/27.
+//  Copyright © 2020 akrolayer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
